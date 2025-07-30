887634e53547428b553785fe0a37ae1ae174410e
MichaelaPop <mesha.spoluprace@gmail.com>
2025-07-30 17:24:33 +0200

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyCweM-p_XTzZlgMX89xDW2umG0pxfo3WRQ",
-  authDomain: "tinky-rozlucka.firebaseapp.com",
-  projectId: "tinky-rozlucka",
-  storageBucket: "tinky-rozlucka.firebasestorage.app",
-  messagingSenderId: "756403512507",
-  appId: "1:756403512507:web:53c0af432b78e925b1b59e",
-  measurementId: "G-XNQRVV2KW5"
-};
